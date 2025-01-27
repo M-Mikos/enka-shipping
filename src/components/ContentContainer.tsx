93037b0d581bfbd8b838af341b23b4cfecfdc522
M-Mikos <mikos.marcin.m@gmail.com>
2025-01-27 12:25:21 +0100

    Update Tailwind CSS

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -5,5 +5,5 @@ interface ContentContainerProps {
 }
 
 export default function ContentContainer({ children }: ContentContainerProps) {
-  return <div className="w-full">{children}</div>;
+  return <div className="w-full md:w-3xl lg:w-5xl xl:w-7xl 2xl: ">{children}</div>;
 }
