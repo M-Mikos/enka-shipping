77ecb6b915b88df58932f861174fc8a54b2476cb
M-Mikos <mikos.marcin.m@gmail.com>
2025-01-27 13:51:52 +0100

    Refactor styles in ContentContainer

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -5,5 +5,5 @@ interface ContentContainerProps {
 }
 
 export default function ContentContainer({ children }: ContentContainerProps) {
-  return <div className="w-full md:w-3xl lg:w-5xl xl:w-7xl 2xl: ">{children}</div>;
+  return <div className="mx-auto px-6 w-full md:container">{children}</div>;
 }
