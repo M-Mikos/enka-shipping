9ff64f30707313ce3688169f9a5816bf5a3c07ce
M-Mikos <mikos.marcin.m@gmail.com>
2025-01-28 09:25:32 +0100

    Add logo

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -1,13 +1,20 @@
 "use Client";
 
+import { useTranslations } from "next-intl";
+import Image from "next/image";
 import ContentContainer from "../ContentContainer";
+import logo from "../../../public/logo-enka.svg";
 
 export default function Header() {
+  const t = useTranslations("Global");
+
   return (
     <header>
       <ContentContainer>
         <div className="flex justify-between items-center h-20 py-4">
-          <div>Logo</div>
+          <div className="">
+            <Image src={logo} width="120" height="30" alt={t("logoAlt")} />
+          </div>
           <div className="flex items-center gap-12">
             <nav>
               <ul className="flex items-center gap-3">
