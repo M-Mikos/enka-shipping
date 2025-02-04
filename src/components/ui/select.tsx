081694ed96c24ff3761aa079083c7d736d1c82b0
M-Mikos <mikos.marcin.m@gmail.com>
2025-02-04 12:21:28 +0100

    Add background hover style to select trigger

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -19,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border-slate-200 bg-transparent px-3 py-2 text-sm ring-offset-white text-slate-600 placeholder:text-slate-400 focus:outline-none focus:ring-1 focus:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 dark:border-slate-800 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus:ring-slate-300 cursor-pointer",
+      "flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border-slate-200 bg-transparent px-3 py-2 text-sm ring-offset-white transition-colors text-slate-600 placeholder:text-slate-400 hover:bg-slate-100 focus:outline-none focus:ring-1 focus:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 dark:border-slate-800 dark:hover:bg-slate-800 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus:ring-slate-300 cursor-pointer",
       className
     )}
     {...props}
