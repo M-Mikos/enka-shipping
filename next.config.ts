9ee19491db7b4713662a6d0145005f2cb64e1c8a
M-Mikos <mikos.marcin.m@gmail.com>
2025-02-10 11:27:06 +0100

    Debud vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
+import fs from "fs";
 import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    // Log files in the src directory
+    console.log("Files in src:", fs.readdirSync(path.resolve(__dirname, "src")));
     return config;
   },
 };
