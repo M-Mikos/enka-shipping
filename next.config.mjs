da80bfc65bad3f8ad0362842015121bcc1a89d59
M-Mikos <mikos.marcin.m@gmail.com>
2025-02-12 11:45:11 +0100

    Add custom image optimisation device sizes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    deviceSizes: [828, 1080, 1200, 1920, 2048],
+  },
+};
 
 export default withNextIntl(nextConfig);
