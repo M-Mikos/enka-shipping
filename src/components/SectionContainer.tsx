56b04d2301c82ceff83651a6f9ff43f529894e4c
M-Mikos <mikos.marcin.m@gmail.com>
2025-02-12 11:47:58 +0100

    Add SectionContainer component

---
+++ src/components/SectionContainer.tsx
@@ -0,0 +1,38 @@
+import Image, { StaticImageData } from "next/image";
+
+interface SectionContainerProps extends React.HTMLAttributes<HTMLElement> {
+  backgroundImage?: {
+    src: StaticImageData;
+    width: number;
+    height: number;
+    alt: string;
+    classes?: string;
+    priority?: boolean;
+  };
+}
+
+async function SectionContainer({ id, children, className, backgroundImage, ...props }: SectionContainerProps) {
+  return (
+    <section id={id} className={"relative w-screen " + (className ? className : "")} {...props}>
+      {children}
+      {backgroundImage ? (
+        <Image
+          className={
+            "absolute inset-0 w-full h-full object-cover -z-1 " +
+            (backgroundImage.classes ? backgroundImage.classes : "")
+          }
+          src={backgroundImage.src}
+          width={backgroundImage.width}
+          height={backgroundImage.height}
+          alt={backgroundImage.alt}
+          priority={backgroundImage.priority}
+          sizes="100vw"
+        />
+      ) : (
+        ""
+      )}
+    </section>
+  );
+}
+
+export default SectionContainer;
