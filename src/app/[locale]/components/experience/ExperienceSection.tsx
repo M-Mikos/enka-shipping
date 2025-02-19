9157a7eb381f8f8606fec3e90a47e02e20ae7e2a
M-Mikos <mikos.marcin.m@gmail.com>
2025-02-19 12:03:40 +0100

    Add Experience Section component and translations

---
+++ src/app/[locale]/components/experience/ExperienceSection.tsx
@@ -0,0 +1,12 @@
+import ContentContainer from "@/components/ContentContainer";
+import SectionContainer from "@/components/SectionContainer";
+
+function ExperienceSection() {
+  return (
+    <SectionContainer id="experience">
+      <ContentContainer></ContentContainer>
+    </SectionContainer>
+  );
+}
+
+export default ExperienceSection;
