409d95d4bff748f577719ff12169b9be37816b89
M-Mikos <mikos.marcin.m@gmail.com>
2025-02-19 12:12:50 +0100

    Add RichText component

---
+++ src/components/RichText.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+// These tags are available
+type Tag = "br" | "strong";
+
+type Props = {
+  children(tags: Record<Tag, (chunks: ReactNode) => ReactNode>): ReactNode;
+};
+
+export default function RichText({ children }: Props) {
+  return (
+    <div className="prose">
+      {children({
+        br: () => <br />,
+        strong: (chunks: ReactNode) => <strong className="font-bold">{chunks}</strong>,
+      })}
+    </div>
+  );
+}
