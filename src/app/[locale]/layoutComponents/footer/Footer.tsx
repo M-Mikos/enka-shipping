eeb9b24f8920c386824d9cb97aac3004200a8815
M-Mikos <mikos.marcin.m@gmail.com>
2025-02-28 17:09:31 +0100

    Add transparent logo to footer

--- src/app/[locale]/layoutComponents/footer/Footer.tsx
+++ src/app/[locale]/layoutComponents/footer/Footer.tsx
@@ -1,6 +1,8 @@
 import ContentContainer from "@/components/ContentContainer";
 import { Paragraph } from "@/components/ui/paragraph";
 import { useTranslations } from "next-intl";
+import enkaTransparentLogo from "@/../public/logo-enka-dark-transparent.svg";
+import Image from "next/image";
 
 export default function Footer() {
   const t = useTranslations("Footer");
@@ -8,20 +10,22 @@ export default function Footer() {
   return (
     <footer className="bg-slate-950">
       <ContentContainer>
-        <div className="flex justify-between py-12 border-b border-slate-800">
-          <div></div>
+        <div className="flex justify-between items-center py-12 border-b border-slate-800 gap-4">
+          <div className="basis-1/2">
+            <Image src={enkaTransparentLogo} width={500} height={116} alt={t("logoAlt")} unoptimized />
+          </div>
           <div>
             <Paragraph variant="small" shade="light">
               {t("companyName")}
             </Paragraph>
-            <Paragraph variant="small" shade="light">
+            <Paragraph className="text-right" variant="small" shade="light">
               {t("companyAdress")}
             </Paragraph>
           </div>
         </div>
-        <div className="flex justify-between gap-2 py-6 text-sm text-slate-600">
+        <div className="flex justify-between items-center gap-2 py-6 text-sm text-slate-600">
           <span>{t("copyright", { year: year })}</span>
-          <span>
+          <span className="text-right">
             {t("Credits.prefix")}
             <a className="transition hover:text-slate-300" href={t("Credits.link")} title={t("Credits.linkTitle")}>
               {t("Credits.author")}
