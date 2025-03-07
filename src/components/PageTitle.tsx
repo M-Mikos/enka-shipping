9763f6f749b312edd19a73f6bf630d21570f6f75
M-Mikos <mikos.marcin.m@gmail.com>
2025-03-07 13:09:32 +0100

    Add styles to pagetitle component

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -12,17 +12,13 @@ interface PageTitleProps {
 
 function PageTitle({ messages }: PageTitleProps) {
   return (
-    <SectionContainer>
+    <SectionContainer className="bg-slate-50 ">
       <ContentContainer>
-        <div className="flex flex-col gap-8 lg:flex-row lg:items-center lg:gap-16 border-b-2 border-slate-950 py-16 lg:py-24 lg:min-h-72 ">
+        <div className="flex flex-col gap-8 lg:flex-row lg:items-center lg:gap-16 py-12 lg:py-16 lg:min-h-72 ">
           <Heading className="lg:basis-1/2 mb-0 3xl:mb-0" variant="h1">
             {messages.title}
           </Heading>
-          {messages.subtitle && (
-            <Paragraph className="lg:basis-1/2" variant="large">
-              {messages.subtitle}
-            </Paragraph>
-          )}
+          {messages.subtitle && <Paragraph className="lg:basis-1/2">{messages.subtitle}</Paragraph>}
         </div>
       </ContentContainer>
     </SectionContainer>
