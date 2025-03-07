78eef1d4f1d0d62112a5e18a5df1361aebcc3341
M-Mikos <mikos.marcin.m@gmail.com>
2025-03-07 10:24:20 +0100

    Add sitemap.ts file

---
+++ src/app/sitemap.ts
@@ -0,0 +1,45 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          pl: `${process.env.NEXT_PUBLIC_SITE_URL}/pl`,
+          it: `${process.env.NEXT_PUBLIC_SITE_URL}/en`,
+          en: `${process.env.NEXT_PUBLIC_SITE_URL}/it`,
+        },
+      },
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/services`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          pl: `${process.env.NEXT_PUBLIC_SITE_URL}/pl/uslugi`,
+          it: `${process.env.NEXT_PUBLIC_SITE_URL}/en/services`,
+          en: `${process.env.NEXT_PUBLIC_SITE_URL}/it/i-nostri-servizi`,
+        },
+      },
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/realizations`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          pl: `${process.env.NEXT_PUBLIC_SITE_URL}/pl/realizacje`,
+          it: `${process.env.NEXT_PUBLIC_SITE_URL}/en/realizations`,
+          en: `${process.env.NEXT_PUBLIC_SITE_URL}/it/realizzazioni`,
+        },
+      },
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+  ];
+}
