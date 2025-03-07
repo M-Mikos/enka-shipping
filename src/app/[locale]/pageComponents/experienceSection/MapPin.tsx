27a206d36134c0d8abc6c89dbd5c93048193d734
M-Mikos <mikos.marcin.m@gmail.com>
2025-03-07 13:07:55 +0100

    Add links to regions and pins in experience section

--- src/app/[locale]/pageComponents/experienceSection/MapPin.tsx
+++ src/app/[locale]/pageComponents/experienceSection/MapPin.tsx
@@ -1,6 +1,7 @@
 import MapPinIcon from "@/../public/home/map-pin.svg";
+import { Link } from "@/i18n/routing";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
-
 interface MapPinProps {
   region: string;
   coords: string[];
@@ -9,15 +10,23 @@ interface MapPinProps {
 // The styles responsible for bi-directional hover effect connection between MapPin and Region list item has been hardcoded in global.css file due to Tailwind v4 limitations (no Safelist configuration available).
 
 function MapPin({ region, coords }: MapPinProps) {
+  const t = useTranslations("Header");
+  const pathname = "/";
+
   const [left, top] = coords;
   return (
-    <div
-      data-region={region}
-      style={{ left, top }}
-      className={`absolute w-4 h-4 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-6 lg:h-6 xl:w-8 xl:h-8 2xl:w-10 2xl:h-10 transition-transform hover:-translate-y-2`}
+    <Link
+      title={t("Navigation.ContactLink.linkTitle")}
+      href={{ pathname: pathname, hash: t("Navigation.ContactLink.link") }}
     >
-      <Image className="w-full h-full" src={MapPinIcon} width="50" height="50" alt="" unoptimized />
-    </div>
+      <div
+        data-region={region}
+        style={{ left, top }}
+        className={`absolute w-4 h-4 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-6 lg:h-6 xl:w-8 xl:h-8 2xl:w-10 2xl:h-10 transition-transform hover:-translate-y-2`}
+      >
+        <Image className="w-full h-full" src={MapPinIcon} width="50" height="50" alt="" unoptimized />
+      </div>
+    </Link>
   );
 }
 
--- src/app/[locale]/pageComponents/experienceSection/Region.tsx
+++ src/app/[locale]/pageComponents/experienceSection/Region.tsx
@@ -1,3 +1,5 @@
+import { Link } from "@/i18n/routing";
+import { useTranslations } from "next-intl";
 import { ReactNode } from "react";
 
 interface RegionProps {
@@ -8,9 +10,17 @@ interface RegionProps {
 // The styles responsible for bi-directional hover effect connection between MapPin and Region list item has been hardcoded in global.css file due to Tailwind v4 limitations (no Safelist configuration available).
 
 function Region({ region, children }: RegionProps) {
+  const t = useTranslations("Header");
+  const pathname = "/";
   return (
-    <li data-region={region} className={`basis-1/2 flex gap-4 hover:text-primary hover:underline`}>
-      {children}
+    <li data-region={region} className={`basis-1/2 `}>
+      <Link
+        title={t("Navigation.ContactLink.linkTitle")}
+        href={{ pathname: pathname, hash: t("Navigation.ContactLink.link") }}
+        className="flex gap-4 hover:text-primary hover:underline"
+      >
+        {children}
+      </Link>
     </li>
   );
 }
