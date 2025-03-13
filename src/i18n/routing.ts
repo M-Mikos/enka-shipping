d3913a94844eb13725ed9c4f2d32e8fdfd04c55d
M-Mikos <mikos.marcin.m@gmail.com>
2025-03-13 10:34:59 +0100

    Add german translation

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,7 +3,7 @@ import { createNavigation } from "next-intl/navigation";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["pl", "en", "it"],
+  locales: ["pl", "en", "it", "de"],
 
   // Used when no locale matches
   defaultLocale: "pl",
@@ -13,16 +13,19 @@ export const routing = defineRouting({
       pl: "/",
       en: "/",
       it: "/",
+      de: "/",
     },
     "/services": {
       pl: "/uslugi",
       en: "/services",
       it: "/i-nostri-servizi",
+      de: "/angebot",
     },
     "/realizations": {
       pl: "/realizacje",
       en: "/realizations",
       it: "/realizzazioni",
+      de: "/realisierungen",
     },
   },
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ export default createMiddleware(routing);
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", "/(pl|en|it)/:path*"],
+  matcher: ["/", "/(pl|en|it|de)/:path*"],
 };
