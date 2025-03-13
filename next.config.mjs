f1a0679905d48236f68fcff225cf676f3d12a66d
M-Mikos <mikos.marcin.m@gmail.com>
2025-03-13 09:17:20 +0100

    Add new imageSizes set

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    deviceSizes: [400, 600, 750, 1080, 1280, 1920, 2560],
+    deviceSizes: [480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
 };
 
