bdf1fff391bfd4923b38100003599da9e2f4d582
M-Mikos <mikos.marcin.m@gmail.com>
2025-03-24 14:01:43 +0100

    Add service pages

---
+++ src/app/[locale]/services/maritime-transport/page.tsx
@@ -0,0 +1,33 @@
+import { useTranslations } from "next-intl";
+import ContactSection from "../../_components/page-components/contactSection/ContactSection";
+import PageTitle from "@/components/PageTitle";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+import { generatePageMetadata } from "@/lib/utils";
+import OfferItem from "../_components/OfferItem";
+import imageCranes from "@/../public/services/project-cargo/projectcargo-cranes.avif";
+import imagePlatforms from "@/../public/services/project-cargo/projectcargo-platforms.avif";
+import imageRealization from "@/../public/services/project-cargo/projectcargo-realization.avif";
+import imageSpmt from "@/../public/services/project-cargo/projectcargo-spmt.avif";
+import imageSupport from "@/../public/services/project-cargo/projectcargo-support.avif";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations("MaritimeTransportPage.Metadata");
+  return generatePageMetadata(t);
+}
+
+export default function MaritimeTransport() {
+  const t = useTranslations("MaritimeTransportPage");
+  const offerKeys = ["Services", "Oversize", "Bulk", "Intermodal", "Port", "Routes"];
+  const listImages = [imageCranes, imageSpmt, imagePlatforms, imageSupport, imageRealization, imageRealization];
+
+  return (
+    <>
+      <PageTitle messages={{ title: t("title"), subtitle: t("subtitle") }} />
+      {offerKeys.map((key, i) => (
+        <OfferItem key={key} pageKey="MaritimeTransport" translationsKey={key} image={listImages[i]} />
+      ))}
+      <ContactSection />
+    </>
+  );
+}
