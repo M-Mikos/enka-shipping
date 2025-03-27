ad48e202472febcf9e26277dc9936402cd69b260
M-Mikos <mikos.marcin.m@gmail.com>
2025-03-27 13:41:45 +0100

    Update header nav to service pages structure

--- src/app/[locale]/_components/layout-components/header/Navigation.tsx
+++ src/app/[locale]/_components/layout-components/header/Navigation.tsx
@@ -1,12 +1,15 @@
 import { useTranslations } from "next-intl";
 import {
   NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { Link, usePathname } from "@/i18n/routing";
+import clsx from "clsx";
 
 interface NavigationProps {
   orientation: "horizontal" | "vertical";
@@ -23,17 +26,147 @@ function Navigation({ orientation, handleClose }: NavigationProps) {
   return (
     <NavigationMenu className={navigationMenuOrientationClasses} orientation={orientation}>
       <NavigationMenuList className={navigationMenuListOrientationClasses}>
-        <NavigationMenuItem>
-          <Link title={t("Navigation.ServicesLink.linkTitle")} href="/services" legacyBehavior passHref>
-            <NavigationMenuLink
-              className={navigationMenuTriggerStyle()}
-              onClick={handleClose}
-              active={pathname === "/services"}
-            >
-              {t("Navigation.ServicesLink.label")}
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
+        {orientation === "vertical" ? (
+          <>
+            <NavigationMenuItem>
+              <Link
+                href="/services/project-cargo"
+                title={t("Navigation.Services.ServicesContent.ProjectCargoLink.linkTitle")}
+                legacyBehavior
+                passHref
+              >
+                <NavigationMenuLink
+                  className={clsx(navigationMenuTriggerStyle(), "m-1")}
+                  onClick={handleClose}
+                  active={pathname === "/services/project-cargo"}
+                >
+                  {t("Navigation.Services.ServicesContent.ProjectCargoLink.label")}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link
+                href="/services/inland-transport"
+                title={t("Navigation.Services.ServicesContent.InlandTransportLink.linkTitle")}
+                legacyBehavior
+                passHref
+              >
+                <NavigationMenuLink
+                  className={clsx(navigationMenuTriggerStyle(), "m-1")}
+                  onClick={handleClose}
+                  active={pathname === "/services/inland-transport"}
+                >
+                  {t("Navigation.Services.ServicesContent.InlandTransportLink.label")}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link
+                href="/services/land-transport"
+                title={t("Navigation.Services.ServicesContent.LandTransportLink.linkTitle")}
+                legacyBehavior
+                passHref
+              >
+                <NavigationMenuLink
+                  className={clsx(navigationMenuTriggerStyle(), "m-1")}
+                  onClick={handleClose}
+                  active={pathname === "/services/land-transport"}
+                >
+                  {t("Navigation.Services.ServicesContent.LandTransportLink.label")}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link
+                href="/services/maritime-transport"
+                title={t("Navigation.Services.ServicesContent.MaritimeTransportLink.linkTitle")}
+                legacyBehavior
+                passHref
+              >
+                <NavigationMenuLink
+                  className={clsx(navigationMenuTriggerStyle(), "m-1")}
+                  onClick={handleClose}
+                  active={pathname === "/services/maritime-transport"}
+                >
+                  {t("Navigation.Services.ServicesContent.MaritimeTransportLink.label")}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </>
+        ) : (
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>{t("Navigation.Services.label")}</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul>
+                <li>
+                  <Link
+                    href="/services/project-cargo"
+                    title={t("Navigation.Services.ServicesContent.ProjectCargoLink.linkTitle")}
+                    legacyBehavior
+                    passHref
+                  >
+                    <NavigationMenuLink
+                      className={clsx(navigationMenuTriggerStyle(), "m-1")}
+                      onClick={handleClose}
+                      active={pathname === "/services/project-cargo"}
+                    >
+                      {t("Navigation.Services.ServicesContent.ProjectCargoLink.label")}
+                    </NavigationMenuLink>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/services/inland-transport"
+                    title={t("Navigation.Services.ServicesContent.InlandTransportLink.linkTitle")}
+                    legacyBehavior
+                    passHref
+                  >
+                    <NavigationMenuLink
+                      className={clsx(navigationMenuTriggerStyle(), "m-1")}
+                      onClick={handleClose}
+                      active={pathname === "/services/inland-transport"}
+                    >
+                      {t("Navigation.Services.ServicesContent.InlandTransportLink.label")}
+                    </NavigationMenuLink>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/services/land-transport"
+                    title={t("Navigation.Services.ServicesContent.LandTransportLink.linkTitle")}
+                    legacyBehavior
+                    passHref
+                  >
+                    <NavigationMenuLink
+                      className={clsx(navigationMenuTriggerStyle(), "m-1")}
+                      onClick={handleClose}
+                      active={pathname === "/services/land-transport"}
+                    >
+                      {t("Navigation.Services.ServicesContent.LandTransportLink.label")}
+                    </NavigationMenuLink>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/services/maritime-transport"
+                    title={t("Navigation.Services.ServicesContent.MaritimeTransportLink.linkTitle")}
+                    legacyBehavior
+                    passHref
+                  >
+                    <NavigationMenuLink
+                      className={clsx(navigationMenuTriggerStyle(), "m-1")}
+                      onClick={handleClose}
+                      active={pathname === "/services/maritime-transport"}
+                    >
+                      {t("Navigation.Services.ServicesContent.MaritimeTransportLink.label")}
+                    </NavigationMenuLink>
+                  </Link>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        )}
+
         <NavigationMenuItem>
           <Link title={t("Navigation.RealizationsLink.linkTitle")} href="/realizations" legacyBehavior passHref>
             <NavigationMenuLink
